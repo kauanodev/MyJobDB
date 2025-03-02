1d82d03a59461def54621187f9d9d819ae352eb5
Wesley Alves Rolim <walvesrolim@gmail.com>
2025-03-02 16:37:34 -0300

    🐛 missing sequence

--- tables/002_servico.sql
+++ tables/002_servico.sql
@@ -2,10 +2,15 @@
 
 -- DROP TABLE IF EXISTS public."Servico";
 
+CREATE SEQUENCE IF NOT EXISTS servico_id_seq;
+
 CREATE TABLE IF NOT EXISTS public."Servico"
 (
-    id integer NOT NULL DEFAULT nextval('"Servico_id_seq"'::regclass),
+    id integer NOT NULL DEFAULT nextval('"servico_id_seq"'::regclass),
     nome text COLLATE pg_catalog."default" NOT NULL,
     categoria text COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "Servico_pkey" PRIMARY KEY (id)
 );
+
+ALTER SEQUENCE servico_id_seq
+OWNED BY public."Servico".id;
