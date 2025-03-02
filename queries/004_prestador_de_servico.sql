59620e280eb50427ee993b5fa59fc5eb28b95945
Wesley Alves Rolim <walvesrolim@gmail.com>
2025-03-02 16:37:15 -0300

    🐛 fix queires for 004_prestador_de_service.sql

--- queries/004_prestador_de_servico.sql
+++ queries/004_prestador_de_servico.sql
@@ -1,15 +1,27 @@
+--SELECT_MANY
+SELECT * FROM public."Usuario" AS u
+LEFT JOIN public."PrestadorDeServico" AS s ON u.id = s.user_id
+--END-SELECT_MANY
+
+--SELECT
 SELECT * FROM public."Usuario" AS u 
 LEFT JOIN public."PrestadorDeServico" AS s ON u.id = s.user_id
 WHERE u.id = %s
+--END-SELECT
 
+--INSERT
 INSERT INTO public."PrestadorDeServico" (user_id, additional_info)
 VALUES (%s, %s);
+--END-INSERT
 
+--UPDATE
 UPDATE public."PrestadorDeServico"
 SET 
-    user_id = %s,
     additional_info = %s
 WHERE user_id = %s;
+--END-UPDATE
 
+--DELETE
 DELETE FROM public."PrestadorDeServico"
 WHERE user_id = %s;
+--END-DELETE
