d8f5efb8fdc0a700aeafed72ad71359de4d97603
mgabrielalima <margabrielalima@gmail.com>
2025-03-04 22:18:20 -0300

    ✨ Queries de usuário e contratante

---
+++ queries/001_usuario.sql
@@ -0,0 +1,28 @@
+-- SELECT_MANY
+SELECT * FROM public."Usuario";
+--END-SELECT_MANY
+
+-- SELECT
+SELECT * FROM public."Usuario"
+WHERE id = %s;
+--END-SELECT
+
+-- INSERT
+INSERT INTO public."Usuario" (id, cpf, endereco, nome, data_de_nascimento)
+VALUES (%s, %s, %s, %s, %s);
+--END-INSERT
+
+-- UPDATE
+UPDATE public."Usuario"
+SET
+    cpf = %s,
+    endereco = %s,
+    nome = %s,
+    data_de_nascimento = %s
+WHERE id = %s;
+--END-UPDATE
+
+-- DELETE
+DELETE FROM public."Usuario"
+WHERE id = %s;
+--END-DELETE
